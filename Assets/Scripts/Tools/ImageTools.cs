432c8ae8cc59a45eb51aa17149b1b23405abd5c4
VirtualBrightPlayz <lego.trains.b@gmail.com>
2025-02-27 23:46:54 -0700

    optimize imagetools and quickinvoke

--- Assets/Scripts/Tools/ImageTools.cs
+++ Assets/Scripts/Tools/ImageTools.cs
@@ -11,25 +11,8 @@ namespace Hypernex.Tools
         {
             if (!ignoreCache && CachedImages.ContainsKey(id))
                 return CachedImages[id];
-            /*MemoryStream ms = new MemoryStream(bytes);
-            Bitmap b = new Bitmap(ms);
-            Texture2D t = new Texture2D(b.Width, b.Height);
-            for (int x = 0; x < b.Width; x++)
-            {
-                for (int y = 0; y < b.Height; y++)
-                {
-                    System.Drawing.Color pixelColor = b.GetPixel(x, y);
-                    UnityEngine.Color unity_pixelColor =
-                        new UnityEngine.Color(pixelColor.R / 255.0f, pixelColor.G / 255.0f, 
-                            pixelColor.B / 255.0f, pixelColor.A / 255.0f);
-                    t.SetPixel(x, b.Height - y, unity_pixelColor);
-                }
-            }
-            b.Dispose();
-            ms.Dispose();*/
-            Texture2D t = new Texture2D(1,1);
+            Texture2D t = new Texture2D(512, 512, TextureFormat.RGBA32, false);
             t.LoadImage(bytes);
-            t.Apply();
             CachedImages.Add(id, t);
             return t;
         }
--- Assets/Scripts/Tools/QuickInvoke.cs
+++ Assets/Scripts/Tools/QuickInvoke.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Hypernex.Tools
 {
-    public static class QuickInvoke
+    public class QuickInvoke : MonoBehaviour
     {
-        public static void InvokeActionOnMainThread(object action, params object[] args) => UnityMainThreadDispatcher
-            .Instance().Enqueue(() => action.GetType().GetMethod("Invoke").Invoke(action, args));
+        public static void InvokeActionOnMainThread(Delegate action, params object[] args) => queue.Enqueue((action, args));
         
-        public static void InvokeActionOnMainThreadObject(object action, object[] args) => UnityMainThreadDispatcher
-            .Instance().Enqueue(() => action.GetType().GetMethod("Invoke").Invoke(action, args));
+        public static void InvokeActionOnMainThreadObject(Delegate action, object[] args) => queue.Enqueue((action, args));
         
         public static void OverwriteListener(UnityEvent u, Action newAction)
         {
@@ -21,6 +22,28 @@ namespace Hypernex.Tools
         {
             u.RemoveAllListeners();
             u.AddListener(newAction.Invoke);
+        }
+
+        public static QuickInvoke Instance;
+        public static ConcurrentQueue<(Delegate, object[])> queue = new ConcurrentQueue<(Delegate, object[])>();
+        private Stopwatch sw = new Stopwatch();
+        public int maxInvokes = 1000;
+
+        private void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        private void Update()
+        {
+            int i = 0;
+            sw.Restart();
+            while (sw.ElapsedTicks < Stopwatch.Frequency / 2000 && i < maxInvokes && queue.TryDequeue(out (Delegate action, object[] args) item))
+            {
+                item.action.DynamicInvoke(item.args);
+                i++;
+            }
         }
     }
 }
